f8b5e504cc1db33055c0eef144fb9245a50654e4
June Sung Lee <juns8522@gmail.com>
2015-10-08 14:08:07 +1100

    Create Constants.java

---
+++ app/src/main/java/com/example/jun/vocamaster/util/Constants.java
@@ -0,0 +1,27 @@
+package com.example.jun.vocamaster.util;
+
+/**
+ * Created by jun on 8/22/14.
+ */
+public class Constants {
+    public static final int NUMBER_OF_CHAPTERS = 60;
+    public static final int CORRECT = 0;
+    public static final int WRONG = 1;
+    public static final String DISPLAY_TYPE1 = "alphabetical";
+    public static final String DISPLAY_TYPE2 = "correct";
+    public static final String DISPLAY_TYPE3 = "wrong";
+    public static final String FAVORITE_TYPE1 = "vocaMaster";
+    public static final String FAVORITE_TYPE2 = "My Vocabulary";
+    public static final String INSTRUCTION_TYPE1 = "vocabulary";
+    public static final String INSTRUCTION_TYPE2 = "favourite";
+    public static final String INSTRUCTION_TYPE3 = "test";
+    public static final String INSTRUCTION_TYPE4 = "statistics";
+    public static final String INSTRUCTION_TYPE5 = "search";
+    public static final String INSTRUCTION_TYPE6 = "share";
+    public static final String INTERNET_DAUM = "http://dic.daum.net/search.do?q=";
+    public static final String INTERNET_NAVER = "http://dic.naver.com/search.nhn?dicQuery=admire&x=0&y=0&query=";
+    public static final String INTERNET_CAMBRIDGE = "http://dictionary.cambridge.org/search/british/?q=";
+    public static final String INTERNET_OXFORD = "http://www.oxforddictionaries.com/definition/english/";
+    public static final String INTERNET_SYNONYM = "http://www.synonym.com/synonyms/";
+    public static final String INTERNET_DICTIONARY = "http://dictionary.reference.com/browse/";
+}
